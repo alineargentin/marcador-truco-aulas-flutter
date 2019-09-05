f3fb3af8b772150a1acbea3295bdb532d13f16c6
Aline Argentin <alineroberta.argentin@gmail.com>
2019-09-04 21:02:30 -0300

    Implementando desafios
    - Não deixar que seja possível ficar com pontos negativos ao clicar em (-1)
    e também não pode ultrapassar 12 pontos.
    - Permitir de alguma forma que a partida seja reiniciada, sem zerar o
    número de vitórias
    - Transformar o AlertDialog em modal para que somente desapareça da tela ao
    clicar em CANCEL ou OK. Uma dia, precisa utilizar o atributo
    barrierDismissible
    - Trocar os nomes dos usuários ao clicar em cima do nome (Text). Pode-se
    utilizar um GestureDetector e exibir um AlertDialog com um TextField.
    Exemplo de AlertDialog com TextField.
    - Exibir uma notificação da mão de ferro: é a Mão de Onze especial, quando
    as duas duplas conseguem chegar a 11 pontos na partida. Todos os jogadores
    recebem as cartas “cobertas”, isto é, viradas para baixo, e deverão jogar
    assim. Quem vencer a mão, vence a partida
    - Instale o plugin Screen e adicione um código para deixar a tela sempre
    ativa enquanto joga.
    
    Signed-off-by: Aline Argentin <alineroberta.argentin@gmail.com>

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marcador_truco/models/player.dart';
+import 'package:screen/screen.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,11 +11,15 @@ class _HomePageState extends State<HomePage> {
   var _playerOne = Player(name: "Nós", score: 0, victories: 0);
   var _playerTwo = Player(name: "Eles", score: 0, victories: 0);
 
+  TextEditingController _textFieldController = TextEditingController();
+
   @override
   void initState() {
+    // Instale o plugin Screen e adicione um código para deixar a tela sempre ativa enquanto joga:
+    Screen.keepOn(true);
+
     super.initState();
     _resetPlayers();
-    
   }
 
   void _resetPlayer({Player player, bool resetVictories = true}) {
@@ -41,16 +46,31 @@ class _HomePageState extends State<HomePage> {
               _showDialog(
                   title: 'Zerar',
                   message:
-                      'Tem certeza que deseja começar novamente a pontuação?',
+                      'Tem certeza que deseja começar novamente a pontuação? Isso limpará também as vitórias.',
                   confirm: () {
                     _resetPlayers();
-                  });
+                  },
+                  cancel: () {});
             },
             icon: Icon(Icons.refresh),
+          ),
+          IconButton(
+            onPressed: () {
+              _showDialog(
+                  title: 'Iniciar nova partida',
+                  message:
+                      'Deseja iniciar uma nova partida? Isso vai manter as vitórias.',
+                  confirm: () {
+                    _resetPlayers(resetVictories: false);
+                  },
+                  cancel: () {});
+            },
+            icon: Icon(Icons.fiber_new),
           )
         ],
       ),
       body: Container(padding: EdgeInsets.all(20.0), child: _showPlayers()),
+      resizeToAvoidBottomPadding: false,
     );
   }
 
@@ -61,7 +81,7 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          _showPlayerName(player.name),
+          _showPlayerName(player),
           _showPlayerScore(player.score),
           _showPlayerVictories(player.victories),
           _showScoreButtons(player),
@@ -82,13 +102,23 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _showPlayerName(String name) {
-    return Text(
-      name.toUpperCase(),
-      style: TextStyle(
-          fontSize: 22.0,
-          fontWeight: FontWeight.w500,
-          color: Colors.deepOrange),
+  Widget _showPlayerName(Player player) {
+    // Trocar os nomes dos usuários ao clicar em cima do nome (Text).
+    // Pode-se utilizar um GestureDetector e exibir um AlertDialog com um TextField.
+    return GestureDetector(
+      onTap: () {
+        // atualiza texto do controller pra atualizar no Text Field
+        _textFieldController.text = player.name;
+        // mostra o dialog passando o jogador para que seja atualizado
+        _showTextEditDialog(player);
+      },
+      child: Text(
+        player.name.toUpperCase(),
+        style: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.w500,
+            color: Colors.deepOrange),
+      ),
     );
   }
 
@@ -128,6 +158,9 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+// Não deixar que seja possível ficar com pontos negativos ao clicar
+// em (-1) e também não pode ultrapassar 12 pontos.
+
   Widget _showScoreButtons(Player player) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -138,16 +171,25 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black.withOpacity(0.1),
           onTap: () {
             setState(() {
-              player.score--;
+              if (player.score > 0) player.score--;
             });
           },
         ),
+        //MÃO DE FERRO
         _buildRoundedButton(
           text: '+1',
           color: Colors.deepOrangeAccent,
           onTap: () {
             setState(() {
-              player.score++;
+              if (player.score < 12) player.score++;
+              if (_playerOne.score == 11 && _playerTwo.score == 11)
+                _showDialog(
+                  title: 'MÃO DE FERRO',
+                  message: 'Todas as cartas devem estar encobertas!',
+                  confirm: () {
+                    setState(() {});
+                  },
+                );
             });
 
             if (player.score == 12) {
@@ -173,22 +215,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //Transformar o AlertDialog em modal
+  //para que somente desapareça da tela ao clicar em CANCEL ou OK. Uma dia
+  //, precisa utilizar o atributo barrierDismissible
   void _showDialog(
       {String title, String message, Function confirm, Function cancel}) {
     showDialog(
+      barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(title),
           content: Text(message),
           actions: <Widget>[
-            FlatButton(
-              child: Text("CANCEL"),
-              onPressed: () {
-                Navigator.of(context).pop();
-                if (cancel != null) cancel();
-              },
-            ),
+            // if ternário para não exibir botão de cancelar se não receber a função
+            cancel != null
+                ? FlatButton(
+                    child: Text("CANCEL"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      if (cancel != null) cancel();
+                    },
+                  )
+                : null,
             FlatButton(
               child: Text("OK"),
               onPressed: () {
@@ -200,5 +249,33 @@ class _HomePageState extends State<HomePage> {
         );
       },
     );
+  }
+
+  /// Exibe um texto para edição e atualiza o nome do player com o texto inserido
+  void _showTextEditDialog(Player player) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Editar nome'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Digite o nome"),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('OK'),
+                onPressed: () {
+                  if (_textFieldController.text.length > 0) {
+                    setState(() {
+                      player.name = _textFieldController.text;
+                    });
+                  }
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 }
